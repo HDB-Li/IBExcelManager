bf0c5266a8369e83361eecea38d98fd2b0c86329
HDB-Li <llworkinggroup@qq.com>
2021-12-20 22:54:29 +0800

    0.1.1

--- Classes/IBExcelManager.m
+++ Classes/IBExcelManager.m
@@ -14,19 +14,56 @@
         return result;
     }
     
+    NSArray *columns = @[@"A",@"B",@"C",@"D",@"E",@"F",@"G",@"H",@"I",@"J",@"K",@"L",@"M",@"N",@"O",@"P",@"Q",@"R",@"S",@"T",@"U",@"V",@"W",@"X",@"Y",@"Z"];
+    NSString *symbolCol = nil;
+    NSString *typeCol = nil;
+    NSString *exchangeCol = nil;
+    NSString *primExchCol = nil;
+    NSString *currencyCol = nil;
+    NSString *actionCol = nil;
+    NSString *quantityCol = nil;
+    NSString *orderTypeCol = nil;
+    
+    for (NSInteger i = 0; i <= 1; i++) {
+        for (NSString *col in columns) {
+            if ([[[sheet getCellWithColumn:col row:i error:nil] stringValue].lowercaseString isEqualToString:@"symbol"]) {
+                symbolCol = col;
+            } else if ([[[sheet getCellWithColumn:col row:i error:nil] stringValue].lowercaseString isEqualToString:@"type"]) {
+                typeCol = col;
+            } else if ([[[sheet getCellWithColumn:col row:i error:nil] stringValue].lowercaseString isEqualToString:@"exchange"]) {
+                exchangeCol = col;
+            } else if ([[[sheet getCellWithColumn:col row:i error:nil] stringValue].lowercaseString isEqualToString:@"prim exch"]) {
+                primExchCol = col;
+            } else if ([[[sheet getCellWithColumn:col row:i error:nil] stringValue].lowercaseString isEqualToString:@"currency"]) {
+                currencyCol = col;
+            } else if ([[[sheet getCellWithColumn:col row:i error:nil] stringValue].lowercaseString isEqualToString:@"action"]) {
+                actionCol = col;
+            } else if ([[[sheet getCellWithColumn:col row:i error:nil] stringValue].lowercaseString isEqualToString:@"quantity"]) {
+                quantityCol = col;
+            } else if ([[[sheet getCellWithColumn:col row:i error:nil] stringValue].lowercaseString isEqualToString:@"order type"]) {
+                orderTypeCol = col;
+            }
+        }
+    }
+    
+    if (!symbolCol || !typeCol || !exchangeCol || !primExchCol || !currencyCol || !actionCol || !quantityCol || !orderTypeCol) {
+        return result;
+    }
+    
     for (NSInteger i = 2; i < maxCount + 2; i++) {
-        NSString *symbol = [[sheet getCellWithColumn:@"A" row:i error:nil] stringValue];
-        NSString *type = [[sheet getCellWithColumn:@"B" row:i error:nil] stringValue];
-        NSString *exchange = [[sheet getCellWithColumn:@"C" row:i error:nil] stringValue];
-        NSString *primExch = [[sheet getCellWithColumn:@"D" row:i error:nil] stringValue];
-        NSString *currency = [[sheet getCellWithColumn:@"E" row:i error:nil] stringValue];
-        NSString *action = [[sheet getCellWithColumn:@"F" row:i error:nil] stringValue];
-        NSInteger quantity = [[[sheet getCellWithColumn:@"G" row:i error:nil] cellDic][@"v"][@"text"] integerValue];
-        NSString *orderType = [[sheet getCellWithColumn:@"H" row:i error:nil] stringValue];
+        NSString *symbol = [[sheet getCellWithColumn:symbolCol row:i error:nil] stringValue];
+        NSString *type = [[sheet getCellWithColumn:typeCol row:i error:nil] stringValue];
+        NSString *exchange = [[sheet getCellWithColumn:exchangeCol row:i error:nil] stringValue];
+        NSString *primExch = [[sheet getCellWithColumn:primExchCol row:i error:nil] stringValue];
+        NSString *currency = [[sheet getCellWithColumn:currencyCol row:i error:nil] stringValue];
+        NSString *action = [[sheet getCellWithColumn:actionCol row:i error:nil] stringValue];
+        NSInteger quantity = [[[sheet getCellWithColumn:quantityCol row:i error:nil] cellDic][@"v"][@"text"] integerValue];
+        NSString *orderType = [[sheet getCellWithColumn:orderTypeCol row:i error:nil] stringValue];
         
-        
-        IBExcelModel *model = [[IBExcelModel alloc] initWithSymbol:symbol secType:type exchange:exchange primaryExchange:primExch currency:currency action:action totalQuantity:quantity orderType:orderType];
-        [result addObject:model];
+        if (symbol.length && type.length && exchange.length && primExch.length && currency.length && action.length && quantity && orderType.length) {
+            IBExcelModel *model = [[IBExcelModel alloc] initWithSymbol:symbol secType:type exchange:exchange primaryExchange:primExch currency:currency action:action totalQuantity:quantity orderType:orderType];
+            [result addObject:model];
+        }
     }
     
     return result;
